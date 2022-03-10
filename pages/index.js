db1f394f5f1aa6e7e383ca27a1b01c5e1512b3dd
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-03-10 13:23:16 -0500

    Final look

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 var icon = document.getElementById("icon");
 var photo = document.getElementById("photo");
+var insta = document.getElementById("insta");
+var git = document.getElementById("github");
+var vk = document.getElementById("vk");
+var fb = document.getElementById("facebook");
 
 icon.onclick = function () {
   document.body.classList.toggle("body_light_theme");
@@ -12,5 +16,25 @@ icon.onclick = function () {
     photo.src = "../images/light-mode-photo.jpg";
   } else {
     photo.src = "../images/intro__headshot.jpg";
+  }
+  if (document.body.classList.contains("body_light_theme")) {
+    insta.src = "../images/insta_icon_light.svg";
+  } else {
+    insta.src = "../images/insta_icon_dark.svg";
+  }
+  if (document.body.classList.contains("body_light_theme")) {
+    git.src = "../images/GitHub_icon_light.svg";
+  } else {
+    git.src = "../images/GitHub_icon_dark.svg";
+  }
+  if (document.body.classList.contains("body_light_theme")) {
+    vk.src = "../images/vk_icon_light.svg";
+  } else {
+    vk.src = "../images/vk_icon_dark.svg";
+  }
+  if (document.body.classList.contains("body_light_theme")) {
+    fb.src = "../images/facebook_icon_light.svg";
+  } else {
+    fb.src = "../images/facebook_icon_dark.svg";
   }
 };
